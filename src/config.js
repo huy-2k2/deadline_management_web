971122ff3e83f35990fe6f30a0d418f88bfe02e0
huy-2k2 <huydev2002@gmail.com>
2023-04-01 20:01:04 +0700

    .

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://252f-2405-4800-1f1f-d100-94cd-b420-47b2-64ac.ap.ngrok.io/api'
+const BASE_URL = 'https://3d4f-2405-4803-fee7-7290-8864-a80f-eed7-3d0d.ap.ngrok.io/api'
 const PUSHER_APP_KEY = '2a7395e5789e352bdad0'
 const PUSHER_CLUSTER = 'ap1'
 export {BASE_URL, PUSHER_APP_KEY, PUSHER_CLUSTER}
