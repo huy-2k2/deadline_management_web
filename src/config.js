ae6e1e479c88f41a3b0dbb1847d14cba57cf5bb8
huy-2k2 <huydev2002@gmail.com>
2023-08-07 23:45:12 +0700

    test

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://d439-210-245-52-125.ngrok-free.app/api'
+const BASE_URL = 'https://d439-210-245-52-125.ngrok-free.app/apisầqwe'
 const PUSHER_APP_KEY = '2a7395e5789e352bdad0'
 const PUSHER_CLUSTER = 'ap1'
 export {BASE_URL, PUSHER_APP_KEY, PUSHER_CLUSTER}
